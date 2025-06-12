50bc8eb739434ecbb7e3d2fa5fe7fc5642019301
chocopiw <sofynurkholifah8@gmail.com>
2025-06-12 13:18:58 +0700

    Membuat Branch mvc_cms_sederhana
    Membuat mvc sederhana dicms sederhana

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__ . '/../models/HomeModel.php';
 class HomeController {
     public function index() {
+        require_once __DIR__ . '/../models/HomeModel.php';
         $model = new HomeModel();
         $data = $model->getWelcomeMessage();
         include __DIR__ . '/../views/home.php';
